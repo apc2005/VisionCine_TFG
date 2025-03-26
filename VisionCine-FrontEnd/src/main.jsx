453c83aa3642469a1f8939b771fa435e2217bc97
apc2005 <160582816+apc2005@users.noreply.github.com>
2025-03-26 10:20:41 +0100

    Update main.jsx

--- VisionCine-FrontEnd/src/main.jsx
+++ VisionCine-FrontEnd/src/main.jsx
@@ -8,7 +8,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}> {/* Asegúrate que este esté envolviendo tu Router y App */}
+  <QueryClientProvider client={queryClient}> 
     <Router>
       <App />
     </Router>
